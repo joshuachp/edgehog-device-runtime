e760761be0acb73bb6d8712f6e5df3bd87364dc0
Joshua Chapman <joshua.chapman@secomind.com>
2026-09-18 16:38:20 +0200

    feat(config): dynamically configure the device runtime
    Signed-off-by: Joshua Chapman <joshua.chapman@secomind.com>

--- scripts/ci/generate-proto.sh
+++ scripts/ci/generate-proto.sh
@@ -48,3 +48,6 @@ cargo run -p proto-codegen --locked -- \
 mv -v \
     "$workingDir/runtime/edgehog.deviceruntime.containers.v1.rs" \
     "$root_dir/edgehog-device-runtime-proto/src/containers/v1.rs"
+mv -v \
+    "$workingDir/runtime/edgehog.deviceruntime.config.v1.rs" \
+    "$root_dir/edgehog-device-runtime-proto/src/config/v1.rs"
